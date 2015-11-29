d2f6e1cd86fb00463665ae1194f8c69580d95d01
aandrioli <andrioliti@gmail.com>
2015-11-29 23:24:50 +0000

    estrutura mvc

---
+++ public/javascripts/app/home/home.controller.js
@@ -0,0 +1,4 @@
+angular.module('agenda')
+.controller('homeController', function($scope) {
+  
+});
---
+++ public/javascripts/app/home/home.js
@@ -0,0 +1,11 @@
+angular.module('agenda')
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/template/home/home.html',
+            controller: 'homeController'
+    });
+    
+    $urlRouterProvider.otherwise('home');
+});
